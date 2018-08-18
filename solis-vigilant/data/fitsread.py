aef7c107897a5940a269efde7c0f758905b77102
andrekorol <anrobits@yahoo.com.br>
2018-08-17 23:33:27 -0300

    Remove decompress_gzip_file and implement the print_fits_hdul_info method

--- solis-vigilant/data/fitsread.py
+++ solis-vigilant/data/fitsread.py
@@ -1,6 +1,7 @@
-import gzip
+import os
 import urllib.error
 import urllib.request
+from astropy.io import fits
 
 
 def download_from_url(url):
@@ -18,32 +19,21 @@ def download_from_url(url):
     return downloaded_file[0]
 
 
-def decompress_gzip_file(filename):
-    """Assumes: filename is a string
-
-    Returns the name of the decompressed file"""
-    file_extension = filename.split('.')[-1]
-    if file_extension == 'gz':
-        with gzip.open(filename, 'rb') as f:
-            file_content = f.read()
-        decompressed_file = '.'.join(filename.split('.')[:-1])
-        with open(decompressed_file, 'wb') as f:
-            f.write(file_content)
-    else:
-        print(f'{filename} is not a gzipped file')
-        return
-    return decompressed_file
-
-
 class FitsFile(object):
     """Class for reading FITS files"""
     def __init__(self, filename):
         self.filename = filename
+
+    def print_fits_hdul_info(self):
+        with fits.open(self.filename) as hdul:
+            hdul.info()
 
 
 file = download_from_url('http://soleil80.cs.technik.fhnw.ch/solarradio/data/2002-20yy_Callisto/2011/08/09/BLEN7M_'
                          '20110809_083004_24.fit.gz')
 print(file)
 
-decompressed_file_gz = decompress_gzip_file(file)
-print(decompressed_file_gz)
+fits_test_file = FitsFile(file)
+fits_test_file.print_fits_hdul_info()
+
+os.remove(file)
