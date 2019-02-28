a4e990442cf5357fce15f16240c51436791f79eb
andrekorol <anrobits@yahoo.com.br>
2019-02-28 18:11:59 -0300

    Change import statement
    Change lib.urlget import statement so that it works with Python 2

--- cycle24.py
+++ cycle24.py
@@ -1,7 +1,7 @@
 import os
-from lib.urlget import download_from_url
+from lib import urlget
 
 # Get list of Solar Cycle 24 top flares and save it in the proper directory
 cycle24_dir = os.path.join("solar-cycles", "cycle24")
-top_flares = download_from_url("http://www.solarham.net/top10.txt", cycle24_dir)
+top_flares = urlget.download_from_url("http://www.solarham.net/top10.txt", cycle24_dir)
 print(top_flares)
