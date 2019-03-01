9e539bf7da620a8fe7489a3bf8d879211d4b7148
andrekorol <anrobits@yahoo.com.br>
2019-02-28 22:30:20 -0300

    Update installation requirements
    Add numpy, astropy and matplotlib to install_requires

--- setup.py
+++ setup.py
@@ -10,13 +10,9 @@ config = {
     'download_url': 'https://github.com/andrekorol/solis-vigilante',
     'author_email': 'anrobits@yahoo.com.br',
     'version': '0.1',
-    'install_requires': ['aplpy', 'astropy'],
     'packages': ['solis-vigilante'],
     'scripts': ['bin/solis-vigilante.py'],
     'name': 'solis-vigilante'
 }
 
-setup(**config)
-
-string = 'http://soleil80.cs.technik.fhnw.ch/solarradio/data/2002-20yy_Callisto/2011/08/09/BLEN7M_20110809_' \
-         '083004_24.fit.g'
+setup(**config, install_requires=['numpy', 'astropy', 'matplotlib'])
