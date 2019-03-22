8a50e2559d27e6b435cf858a9b9dfaf873ebe08e
andrekorol <anrobits@yahoo.com.br>
2019-03-22 17:28:41 -0300

    Add type checking and improve error handling

--- Lib/urlget.py
+++ Lib/urlget.py
@@ -1,24 +1,29 @@
-import urllib.error
+from urllib.error import HTTPError, URLError
 import urllib.request
 from os.path import join, abspath
 from shutil import move
+from socket import timeout
 
 
-def download_from_url(url, save_dir=None):
+def download_from_url(url: str, save_dir: str = ''):
     """
     Downloads the file found at the given url
 
-    :param url: url string of a file to be downloaded
-    :param save_dir: directory to save downloaded file. If omitted, the file is saved in the current working directory
+    :param url: url string of a file to be downloaded.
+    :param save_dir: directory to save downloaded file.
+    If omitted, the file is saved in the current working directory.
     :returns: absolute path of file downloaded from the given url
-    :raises urllib.error.URLError: raises an exception when an invalid url is
-    given as argument
+    :raises urllib.error.URLError: raises an exception when an invalid
+    url is given as argument.
     """
+    assert isinstance(url, str), "{} is not a string".format(url)
+    assert isinstance(save_dir, str), "{} is not a string".format(save_dir)
+
     try:
         downloaded_file = urllib.request.urlretrieve(url, url.split('/')[-1])
         filename = downloaded_file[0]
 
-        if save_dir is not None:
+        if save_dir != '':
             file_path = join(save_dir, filename)
             move(filename, file_path)
             urllib.request.urlcleanup()
@@ -27,7 +32,13 @@ def download_from_url(url, save_dir=None):
         urllib.request.urlcleanup()
         return abspath(filename)
 
-    except urllib.error.URLError as e:
+    except (HTTPError, URLError) as error:
         urllib.request.urlcleanup()
-        print("{}: Name or service not known".format(url))
-        raise e
+        print("Data from {} not retrieved because {}".format(url, error))
+    except ValueError:
+        urllib.request.urlcleanup()
+        print("unknown url type: '{}'".format(url))
+        print(url, "is not a valid url")
+    except timeout:
+        urllib.request.urlcleanup()
+        print("socket timed out - URL {}".format(url))
