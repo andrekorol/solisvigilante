917ee987ce934c1791d15fa4dc5b3b92b8f6b53d
andrekorol <anrobits@yahoo.com.br>
2019-03-22 12:33:42 -0300

    Generate new single and extended plots

--- Tools/extended_plot.py
+++ Tools/extended_plot.py
@@ -1,17 +1,25 @@
 import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from Lib.urlget import download_from_url
 from Lib import fitsread
 
-data_archives = "http://soleil80.cs.technik.fhnw.ch/solarradio/data/2002-20yy_Callisto/2012/08/12/"
-file_list = ["BLEN7M_20120812_140000_25.fit.gz", "BLEN7M_20120812_141500_25.fit.gz", "BLEN7M_20120812_143000_25.fit.gz",
-             "BLEN7M_20120812_144500_25.fit.gz", "BLEN7M_20120812_150000_25.fit.gz", "BLEN7M_20120812_151500_25.fit.gz",
-             "BLEN7M_20120812_153000_25.fit.gz", "BLEN7M_20120812_154500_25.fit.gz"]
+data_archives = "http://soleil80.cs.technik.fhnw.ch/solarradio/data/2002-" \
+                "20yy_Callisto/2012/08/12/"
+file_list = ["BLEN7M_20120812_140000_25.fit.gz",
+             "BLEN7M_20120812_141500_25.fit.gz",
+             "BLEN7M_20120812_143000_25.fit.gz",
+             "BLEN7M_20120812_144500_25.fit.gz",
+             "BLEN7M_20120812_150000_25.fit.gz",
+             "BLEN7M_20120812_151500_25.fit.gz",
+             "BLEN7M_20120812_153000_25.fit.gz",
+             "BLEN7M_20120812_154500_25.fit.gz"]
 
+file_path_list = []
 for file in file_list:
-    fits_file_path = download_from_url('/'.join([data_archives, file]))
-    fits_filename = fits_file_path.split(os.sep)[-1]
-    fits_file = fitsread.ECallistoFitsFile(fits_filename)
-    fits_file.set_file_path()
-    fits_file.set_hdul_dataset()
-    fits_file.plot_freq_range_db_above_background(100, 350)
-    fits_file.delete_file()
+    file_path_list.append(download_from_url('/'.join([data_archives, file])))
+
+
+fitsread.ECallistoFitsFile.plot_fits_files_list(file_path_list, 100, 450,
+                                                'Insert Title Here', 'pt',
+                                                'plot_extendido')
